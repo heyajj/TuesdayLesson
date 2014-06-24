7ef7e0b56b08675353579819ddeb318773978834
heyajj <ajanich@ajanich-macbookpro.roam.corp.google.com>
2014-06-24 10:22:50 -0700

    Initial Commit

---
+++ TuesdayLesson/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TuesdayLesson
+//
+//  Created by Andrew Janich on 6/24/14.
+//  Copyright (c) 2014 Google. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TuesdayLesson/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TuesdayLesson
+//
+//  Created by Andrew Janich on 6/24/14.
+//  Copyright (c) 2014 Google. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
